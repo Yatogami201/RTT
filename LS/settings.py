607b150f6bd17a704bdae4f5ee839229039b2054
Yatogami201 <svalenci41@eafit.edu.co>
2025-05-01 13:46:44 -0500

    db

--- LS/settings.py
+++ LS/settings.py
@@ -94,14 +94,14 @@ WSGI_APPLICATION = 'LS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'vendedor',
-        'USER': 'root',
-        'PASSWORD': 'syn200314',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': 'railway',  # Nombre de la base de datos
+        'USER': 'root',     # Usuario de la base de datos
+        'PASSWORD': 'ncGHCBFglxzbJdRUiHOUOKoKkikhlprU',  # Contraseña
+        'HOST': 'ballast.proxy.rlwy.net',  # Host público de Railway
+        'PORT': '57438',    # Puerto proporcionado por Railway
     }
 }
-
+ 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
